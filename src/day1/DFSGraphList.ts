b4171c991d1c0e95dba5ff35e454bb947f9a9dbe
João <joaog.alves14@gmail.com>
2024-03-15 15:58:32 -0300

    dijkstra

--- src/day1/DFSGraphList.ts
+++ src/day1/DFSGraphList.ts
@@ -1,3 +1,46 @@
-export default function dfs(graph: WeightedAdjacencyList, source: number, needle: number): number[] | null {
+function walk(graph:  WeightedAdjacencyList, curr: number, needle: number, seen: boolean[], path: number[]): boolean {
+  
+  if (curr === needle) {
+    return true;
+  }
+
+  if (seen[curr]) {
+    return false;
+  }
+
+  seen[curr] = true;
+
+  // recurse
+  // pre
+  path.push(curr);
+
+  // recurse
+  const list = graph[curr];
+    
+  for (let i = 0; i < list.length; ++i) {
+    const edge = list[i];
+
+    if (walk(graph, edge.to, needle, seen, path)) {
+      path.push(edge.to);
+      return true;
+    }
+  }
+
+  // post
+  path.pop();
+  
+  return false;
+}
+export default function dfs(
+    graph: WeightedAdjacencyList,
+    source: number,
+    needle: number,
+): number[] | null {
+
+
+  const seen: boolean[] = new Array(graph.lenght).fill(false);
+  const path: number[] = [];
+
+  walk(graph,)
 
 }
